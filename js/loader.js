5652c2dfb2feee4732a180e609c54fa069600849
andyRS <andy337@hotmail.es>
2022-03-21 01:12:35 -0400

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+'use strict'
+
+window.addEventListener('load', ()=>{
+    const contenedorLoader = document.querySelector('.contenedor_loader');
+
+    contenedorLoader.style.opacity= 0;
+    contenedorLoader.style.visibility = 'hidden';
+
+});
