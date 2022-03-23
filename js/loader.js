bb593a83d454eeb1ae5f73a4021bd6bf18916cef
andyRS <andy337@hotmail.es>
2022-03-23 16:55:46 -0400

    Delete js directory

--- js/loader.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-window.addEventListener('load', ()=>{
-    const contenedorLoader = document.querySelector('.contenedor_loader');
-
-    contenedorLoader.style.opacity= 0;
-    contenedorLoader.style.visibility = 'hidden';
-
-});
